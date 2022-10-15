6b08251912dc5957e88f688db9c63f9f8b0ed0ed
Beltz Anhxton <beltz.18kyodai@gmail.com>
2022-10-15 13:40:06 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app     = express()
+const port    = 4000
+const path    = require('path')
+const ejs     = require('ejs')
+
+app.set("views", path.join(__dirname, '../views'))
+app.engine('ejs', ejs.__express)
+app.set('view engine', 'ejs')
+app.use(express.static("views"))
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
+
+app.get('/', (req,res) => {
+  res.render('index')
+})
